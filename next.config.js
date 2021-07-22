37127bc0e0ba67af3e6c9a65a8abe528b7eb300d
Fabien Motte <fabien.motte@algolia.com>
2021-07-22 11:42:58 +0200

    chore: configure nextjs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/** @type {import('next/dist/next-server/server/config').NextConfig} */
+module.exports =  {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'components', 'layouts', 'utils', 'hooks', 'contexts', 'state']
+  },
 }
