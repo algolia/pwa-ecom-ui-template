d94519c3f123ab023ff162ce8c73db8b412b3dff
Fabien Motte <fabien.motte@algolia.com>
2021-07-22 12:06:28 +0200

    feat: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-const Home: React.FC = () => {
-  return <div>Hello world</div>
+import type { GetServerSideProps } from 'next'
+import React from 'react'
+import type { InstantSearchProps } from 'react-instantsearch-dom'
+
+import PageLayout, { getServerSidePropsPage } from '@/layouts/PageLayout'
+
+interface IndexProps {
+  searchState: InstantSearchProps['searchState']
+  resultsState: InstantSearchProps['resultsState']
 }
 
-export default Home
+export default function Index(props: IndexProps): JSX.Element {
+  return (
+    <PageLayout {...props}>
+      <div></div>
+    </PageLayout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = getServerSidePropsPage(
+  Index as React.ComponentType
+)
