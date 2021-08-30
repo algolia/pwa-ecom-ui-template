c8a7d18378cde5e0a5a67aab31177f5e928baa53
Fabien Motte <fabien.motte@algolia.com>
2021-08-30 11:09:48 +0200

    feat: add product-grid items animation

--- components/product/product-grid.tsx
+++ components/product/product-grid.tsx
@@ -1,4 +1,5 @@
-import { m } from 'framer-motion'
+import { AnimatePresence, m } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 import type { ProductCardProps } from './product-card'
 import { ProductCard } from './product-card'
@@ -11,20 +12,51 @@ export type ProductGridProps = {
   products: ProductGridCardProps[]
 }
 
-const transition = {
+const listItemTransition = {
   type: 'spring',
   duration: 0.5,
   bounce: 0.15,
 }
 
+const listItemVariants = {
+  hidden: { opacity: 0 },
+  show: (i: number) => ({
+    opacity: 1,
+    transition: {
+      delay: i * 0.06,
+      duration: 1,
+      ease: [0.16, 1, 0.3, 1],
+    },
+  }),
+}
+
 export function ProductGrid({ products }: ProductGridProps) {
+  const [productsPerPage, setProductsPerPage] = useState(0)
+
+  useEffect(() => {
+    if (!productsPerPage) setProductsPerPage(products.length)
+  }, [productsPerPage, products.length])
+
   return (
-    <ol className="grid grid-cols-2 gap-4 overflow-hidden laptop:grid-cols-5 laptop:gap-6">
-      {products.map(({ objectID, ...props }: ProductGridCardProps) => (
-        <m.li key={objectID} layout="position" transition={transition}>
-          <ProductCard {...props} />
-        </m.li>
-      ))}
-    </ol>
+    <m.ol
+      className="grid grid-cols-2 gap-4 overflow-hidden laptop:grid-cols-5 laptop:gap-6"
+      initial="hidden"
+      animate="show"
+      exit="hidden"
+    >
+      <AnimatePresence>
+        {products.map(({ objectID, ...props }: ProductGridCardProps, i) => (
+          <m.li
+            key={objectID}
+            layout="position"
+            transition={listItemTransition}
+            variants={listItemVariants}
+            custom={i % productsPerPage}
+          >
+            <ProductCard {...props} />
+          </m.li>
+        ))}
+      </AnimatePresence>
+    </m.ol>
   )
 }
