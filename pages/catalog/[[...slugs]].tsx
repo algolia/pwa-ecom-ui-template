ed795f1cff1f4354b930e45b189c8e7c72c61b7f
Fabien Motte <fabien.motte@algolia.com>
2021-09-14 13:18:29 +0200

    fix: mobile banners

--- pages/catalog/[[...slugs]].tsx
+++ pages/catalog/[[...slugs]].tsx
@@ -36,7 +36,7 @@ export default function Catalog(props: SearchPageLayoutProps) {
 
   return (
     <SearchPageLayout {...props}>
-      <Container className="flex flex-col gap-10">
+      <Container className="flex flex-col gap-3 laptop:gap-10">
         <Breadcrumb attributes={breadcrumbAttributes} />
 
         <QueryRuleBanners />
