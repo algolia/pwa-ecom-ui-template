26a76830d9b0dfa02142e1cb2f5a4b646c13b110
Fabien Motte <fabien.motte@algolia.com>
2021-09-15 10:31:43 +0200

    fix: add relevant sort again

--- components/refinements-bar/refinements-bar.tsx
+++ components/refinements-bar/refinements-bar.tsx
@@ -6,6 +6,7 @@ import {
   CurrentRefinements,
   refinementCountAtom,
 } from '@instantsearch/widgets/current-refinements/current-refinements'
+import { RelevantSort } from '@instantsearch/widgets/relevant-sort/relevant-sort'
 import { SortBy } from '@instantsearch/widgets/sort-by/sort-by'
 import { Button } from '@ui/button/button'
 import { Count } from '@ui/count/count'
@@ -43,25 +44,29 @@ export function RefinementsBar({
 
   return (
     <section className={classNames('w-full', className)}>
-      <Tablet className="flex justify-between">
-        <ViewModes />
+      <Tablet className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <ViewModes />
 
-        <Button
-          className="flex items-center gap-1"
-          onClick={() => setMobileExpanded(true)}
-        >
-          <IconLabel
-            icon={FilterIcon}
-            label="Filter &amp; Sort"
-            labelPosition="right"
-            labelTheme="body-regular"
-          />
-          {refinementCount > 0 && <Count>{refinementCount}</Count>}
-        </Button>
+          <Button
+            className="flex items-center gap-1"
+            onClick={() => setMobileExpanded(true)}
+          >
+            <IconLabel
+              icon={FilterIcon}
+              label="Filter &amp; Sort"
+              labelPosition="right"
+              labelTheme="body-regular"
+            />
+            {refinementCount > 0 && <Count>{refinementCount}</Count>}
+          </Button>
+        </div>
+
+        <RelevantSort />
       </Tablet>
 
-      <Laptop className="flex flex-col gap-4">
-        <div className="flex-grow flex gap-6">
+      <Laptop className="flex flex-col items-start gap-4">
+        <div className="w-full flex gap-6">
           {showWidgets && (
             <RefinementsBarDropdowns dynamicWidgets={dynamicWidgets} />
           )}
@@ -80,6 +85,8 @@ export function RefinementsBar({
         </div>
 
         {showWidgets && <CurrentRefinements />}
+
+        <RelevantSort />
       </Laptop>
     </section>
   )
